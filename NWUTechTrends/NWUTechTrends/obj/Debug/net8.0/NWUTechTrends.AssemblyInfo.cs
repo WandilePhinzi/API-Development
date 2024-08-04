77ea4da1e3ea196db354fa531f21ddadc3d3f10a
WC Phinzi <41580117@mynwu.ac.za>
2024-08-04 23:39:25 +0200

    GetMethod Created

--- NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
+++ NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22173ef1cc574853f1c1920a969c48369adb1e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d919c07b5f20d4814636d79a34189f4e4b47c4aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
