858b74ec0cb2ee6cf1421e6206b05c16785590aa
WC Phinzi <41580117@mynwu.ac.za>
2024-08-07 12:04:05 +0200

    Update security

--- NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
+++ NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b58a4b9209c641bdcd1238ded9d2c01125f8e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b004b42c67e436d048a480893263f9ec64cb32d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
