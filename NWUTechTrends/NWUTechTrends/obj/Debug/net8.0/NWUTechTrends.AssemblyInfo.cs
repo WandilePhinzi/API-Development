76e4946ecf21cbcfc39d74af752bd1026594d62d
WC Phinzi <41580117@mynwu.ac.za>
2024-08-08 20:34:58 +0200

    Updates

--- NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
+++ NWUTechTrends/NWUTechTrends/obj/Debug/net8.0/NWUTechTrends.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ffa2435abfdd002bc605e8df25600f767e52dad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076997595eee39233a48cd6aadbc02155f21cbeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NWUTechTrends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
