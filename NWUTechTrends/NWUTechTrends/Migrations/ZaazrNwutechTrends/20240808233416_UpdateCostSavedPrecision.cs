eecb4cc12796c2cd59765d174305579b5e53882f
Wandile Collen <wandilecollen5@gmail.com>
2024-08-09 10:25:53 +0200

    Updates

---
+++ NWUTechTrends/NWUTechTrends/Migrations/ZaazrNwutechTrends/20240808233416_UpdateCostSavedPrecision.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NWUTechTrends.Migrations.ZaazrNwutechTrends
+{
+    /// <inheritdoc />
+    public partial class UpdateCostSavedPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
